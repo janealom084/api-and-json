c19556b9c4b9c22a99cc54a88fca42b40594e6c4
janealom084 <janealom084@gmail.com>
2022-02-22 17:05:18 +0600

    data loded from server

--- api.js
+++ api.js
@@ -3,3 +3,20 @@ function loadData() {
         .then(Response => Response.json())
         .then(data => console.log(data))
 }
+
+function userData() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(Response => Response.json())
+        .then(data => console.log(data))
+}
+
+function loadPost() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(Response => Response.json())
+        .then(post => console.log(post));
+}
+function loadPotos() {
+    fetch('https://jsonplaceholder.typicode.com/photos')
+        .then(Response => Response.json())
+        .then(potos => console.log(potos))
+}
